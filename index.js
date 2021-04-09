c3d04620ebacf4dee5cabdc8d1ee697fe10f9bb3
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2021-04-09 18:19:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ window.onload = function() {
 	const request = new Request('facts/facts.json');
 	fetch(request).then(response => response.json()).then(data => {
 		const facts = data.facts;
-		document.querySelector("input[name='id']").setAttribute("max", facts.length);
-		document.querySelector("input[name='id']").addEventListener("change", function() {
-			document.getElementsByClassName("id").innerHTML = Number(document.querySelector("input[name='id']").value);
-		});
 		document.getElementsByClassName("random")[0].addEventListener("click", function() {
 			if(facts) {
 				location.assign("facts?id=" + Math.floor(Math.random() * facts.length));
