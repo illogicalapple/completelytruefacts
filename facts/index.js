25f51526760a13389dddf03818d26298d711176d
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2021-04-09 19:00:15 -0500

    Update index.js

--- facts/index.js
+++ facts/index.js
@@ -14,7 +14,7 @@ window.onload = function() {
 			.replaceAll("~", "<span class='blue'>")
 			.replaceAll("`", "<span class='bold blue'>");
 		document.getElementsByClassName("random")[0].addEventListener("click", function() {
-			location.assign(window.location.split("?")[0] + "?id=" + Math.floor(Math.random() * facts.length));
+			location.assign(window.location.toString().split("?")[0] + "?id=" + Math.floor(Math.random() * facts.length));
 		});
 	});
 };
