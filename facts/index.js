dc9a14a1154a3ccf870cc6d53be8eb46d597fe7f
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2021-04-12 13:25:47 -0500

    Redirect to existing fact

--- facts/index.js
+++ facts/index.js
@@ -8,7 +8,12 @@ window.onload = function() {
 		const id = escapeHTML.innerHTML
 		document.getElementsByClassName("id")[0].innerHTML = Number(id) + 1;
 		escapeHTML.remove();
-		var fact = facts[id] ? facts[id] : "This fact ~is nonexistent.!";
+		var fact = facts[id] ? facts[id] : "This fact ~is nonexistent.! Redirecting to existing fact in *3 seconds.!";
+		if(!facts[id]) {
+			setTimeout(() => {
+				location.assign("rand");
+			}, 3000);
+		}
 		document.getElementsByClassName("container")[0].innerHTML = fact
 			.replaceAll("!", "</span>")
 			.replaceAll("*", "<span class='big blue'>")
