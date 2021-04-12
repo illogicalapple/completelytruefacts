69bc7a085125eee4c0a22a6a5858b228dd6edd78
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2021-04-12 13:14:28 -0500

    Update random.js

--- facts/rand/random.js
+++ facts/rand/random.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-	const request = new Request("facts.json");
+	const request = new Request("../facts.json");
 	fetch(request).then(response => response.json()).then(data => {
 		location.assign("../?id=" + Math.floor(Math.random() * data.facts.length));
 	});
