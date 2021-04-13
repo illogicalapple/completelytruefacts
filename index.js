a6addf1edffb676751472582621f92ff56e5e4c2
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2021-04-13 13:48:47 -0500

    Fix bug

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 window.onload = () => {
 	document.getElementsByClassName("random")[0].addEventListener("click", function() {
-		if(facts) {
-			location.assign("facts/rand");
-		}
+		location.assign("facts/rand")
 	});
 };
