70ed824236972d8f2e354f52848c2d4bf8d54907
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2021-04-16 13:46:15 -0500

    Update index.js

--- facts/index.js
+++ facts/index.js
@@ -1,27 +1,28 @@
 window.onload = () => {
-	const displayFact = fact => {
+	const displayFact = (fact, index) => {
+		document.getElementsByClassName("id")[0].innerHTML = index + 1;
 		document.getElementsByClassName("container")[0].innerHTML = fact
 			.replaceAll("!", "</span>")
 			.replaceAll("*", "<span class='big blue'>")
 			.replaceAll("~", "<span class='blue'>")
 			.replaceAll("`", "<span class='italic blue'>");
 	}; 
+	var random = undefined;
+	const random = () => {
+		random = Math.floor(Math.random() * facts.length);
+		displayFact(facts[random], random);
+	};
 	const urlParams = new URLSearchParams(window.location.search);
 	if(urlParams.get("id") === undefined) {
-		location.assign("rand")
+		random();
 	}
 	const request = new Request("facts.json");
 	fetch(request).then(response => response.json()).then(data => {
 		const facts = data.facts;
 		document.getElementsByClassName("random")[0].addEventListener("click", function() {
-			displayFact(facts[Math.floor(Math.random() * facts.length)]);
+			random();
 		});
-		var escapeHTML = document.createElement("div"); //creates new element
-		escapeHTML.appendChild(document.createTextNode(urlParams.get("id"))); //createTextNode escapes the string
-		const id = escapeHTML.innerHTML
-		document.getElementsByClassName("id")[0].innerHTML = Number(id) + 1;
-		escapeHTML.remove();
 		var fact = facts[id] ? facts[id] : "This fact ~is nonexistent.!";
-		displayFact(fact)
+		displayFact(fact, urlParams.get("id"));
 	});
 };
