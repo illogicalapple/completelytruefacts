26fda3f2700ec7ccef452fd27bba5f4de8150833
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2021-04-16 13:51:57 -0500

    Update index.js

--- facts/index.js
+++ facts/index.js
@@ -8,21 +8,21 @@ window.onload = () => {
 			.replaceAll("`", "<span class='italic blue'>");
 	}; 
 	var randomNumber = undefined;
-	const random = () => {
-		randomNumber = Math.floor(Math.random() * facts.length);
-		displayFact(facts[randomNumber], randomNumber);
-	};
-	const urlParams = new URLSearchParams(window.location.search);
-	if(urlParams.get("id") === undefined) {
-		random();
-	}
 	const request = new Request("facts.json");
 	fetch(request).then(response => response.json()).then(data => {
+		const random = () => {
+			randomNumber = Math.floor(Math.random() * facts.length);
+			displayFact(facts[randomNumber], randomNumber);
+		};
+		const urlParams = new URLSearchParams(window.location.search);
+		if(urlParams.get("id") === undefined) {
+			random();
+		}
 		const facts = data.facts;
 		document.getElementsByClassName("random")[0].addEventListener("click", function() {
 			random();
 		});
-		var fact = facts[id] ? facts[id] : "This fact ~is nonexistent.!";
+		var fact = facts[urlParams.get("id")] ? facts[urlParams.get("id")] : "This fact ~is nonexistent.!";
 		displayFact(fact, urlParams.get("id"));
 	});
 };
