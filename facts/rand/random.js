49b9e8db695d91a04e35b3b93dd59a83b09ceb5f
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2021-04-16 13:47:13 -0500

    Delete facts/rand directory

--- facts/rand/random.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-	const request = new Request("../facts.json");
-	fetch(request).then(response => response.json()).then(data => {
-		location.assign("../?id=" + Math.floor(Math.random() * data.facts.length));
-	});
-};
