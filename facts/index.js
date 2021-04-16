6d283765f62bba1c3b3bc42ee738407548aea394
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2021-04-16 13:38:38 -0500

    Remove redirect every time a button is pressed

--- facts/index.js
+++ facts/index.js
@@ -1,4 +1,11 @@
-window.onload = function() {
+window.onload = () => {
+	const displayFact = fact => {
+		document.getElementsByClassName("container")[0].innerHTML = fact
+			.replaceAll("!", "</span>")
+			.replaceAll("*", "<span class='big blue'>")
+			.replaceAll("~", "<span class='blue'>")
+			.replaceAll("`", "<span class='italic blue'>");
+	}; 
 	const urlParams = new URLSearchParams(window.location.search);
 	if(urlParams.get("id") === undefined) {
 		location.assign("rand")
@@ -6,24 +13,15 @@ window.onload = function() {
 	const request = new Request("facts.json");
 	fetch(request).then(response => response.json()).then(data => {
 		const facts = data.facts;
+		document.getElementsByClassName("random")[0].addEventListener("click", function() {
+			displayFact(facts[Math.floor(Math.random() * facts.length)]);
+		});
 		var escapeHTML = document.createElement("div"); //creates new element
 		escapeHTML.appendChild(document.createTextNode(urlParams.get("id"))); //createTextNode escapes the string
 		const id = escapeHTML.innerHTML
 		document.getElementsByClassName("id")[0].innerHTML = Number(id) + 1;
 		escapeHTML.remove();
-		var fact = facts[id] ? facts[id] : "This fact ~is nonexistent.! Redirecting to existing fact in *3 seconds.!";
-		if(!facts[id]) {
-			setTimeout(() => {
-				location.assign("rand");
-			}, 3000);
-		}
-		document.getElementsByClassName("container")[0].innerHTML = fact
-			.replaceAll("!", "</span>")
-			.replaceAll("*", "<span class='big blue'>")
-			.replaceAll("~", "<span class='blue'>")
-			.replaceAll("`", "<span class='italic blue'>");
-		document.getElementsByClassName("random")[0].addEventListener("click", function() {
-			location.assign("rand");
-		});
+		var fact = facts[id] ? facts[id] : "This fact ~is nonexistent.!";
+		displayFact(fact)
 	});
 };
